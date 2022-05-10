c6ca677b2bc46e64415d587fd879faa20a3d7e25
Palamarchuk_Tanya <t0973753805@gmail.com>
2022-05-11 02:47:20 +0300

    Доопрацювала з анімацією
    Додала скалу та повороти на картинки продуктів
    
    Працюю над модалкою

---
+++ src/js/modal-products.js
@@ -0,0 +1,19 @@
+// document.getElementById('products__btn').onclick = function() {
+//   document.getElementById('products__card-text').classList.add('products__modal');
+// }
+
+
+// (() => {
+//   const prodBtnRef = document.querySelector("[prod-button]");
+// //   const modalTextRef = document.querySelector("[prod-modal]");
+
+//   prodBtnRef.addEventListener("click", () => {
+//     const expanded =
+//       prodBtnRef.getAttribute("aria-expanded") === "true" || false;
+
+//     prodBtnRef.classList.toggle("products__modal");
+//     prodBtnRef.setAttribute("aria-expanded", !expanded);
+
+//     // modalTextRef.classList.toggle("is-open");
+//   });
+// })();

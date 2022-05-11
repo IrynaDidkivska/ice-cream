2e8274546dd7fe8582b7416ebd40c038b827a1db
Palamarchuk_Tanya <t0973753805@gmail.com>
2022-05-11 17:29:05 +0300

    bugfix/products1
    1. Додала анімацію
    2. Зробила перевертання картки
    
    Не підключила перевертання картки при натискані на кнопку. Ще працюю.

--- src/js/modal-products.js
+++ src/js/modal-products.js
@@ -1,19 +1,15 @@
-// document.getElementById('products__btn').onclick = function() {
-//   document.getElementById('products__card-text').classList.add('products__modal');
-// }
-
-
 // (() => {
-//   const prodBtnRef = document.querySelector("[prod-button]");
-// //   const modalTextRef = document.querySelector("[prod-modal]");
+//   const refs = {
+//     openProdBtn: document.querySelector("[data-modalProd-open]"),
+//     closeProdBtn: document.querySelector("[data-modalProd-close]"),
+//     Prod: document.querySelector("[data-modalProd]"),
+//   };
 
-//   prodBtnRef.addEventListener("click", () => {
-//     const expanded =
-//       prodBtnRef.getAttribute("aria-expanded") === "true" || false;
+//   refs.openProdBtn.addEventListener("click", toggleModal);
+//   refs.closeProdBtn.addEventListener("click", toggleModal);
 
-//     prodBtnRef.classList.toggle("products__modal");
-//     prodBtnRef.setAttribute("aria-expanded", !expanded);
-
-//     // modalTextRef.classList.toggle("is-open");
-//   });
+//   function toggleModal() {
+//     document.body.classList.toggle("modalProd-open");
+//     refs.Prod.classList.toggle("is-hiddenProd");
+//   }
 // })();

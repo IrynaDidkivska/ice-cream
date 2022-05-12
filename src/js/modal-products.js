3edee90f6f389aa34c9bc586c88be5f9426fa0cc
Iryna Didkivska <ididkivska@gmail.com>
2022-05-12 18:07:11 +0300

    update styles

--- src/js/modal-products.js
+++ src/js/modal-products.js
@@ -1,15 +1,15 @@
-// (() => {
-//   const refs = {
-//     openProdBtn: document.querySelector("[data-modalProd-open]"),
-//     closeProdBtn: document.querySelector("[data-modalProd-close]"),
-//     Prod: document.querySelector("[data-modalProd]"),
-//   };
+(() => {
+  const refs = {
+    openProdBtn: document.querySelector("[data-modalProd-open]"),
+    closeProdBtn: document.querySelector("[data-modalProd-close]"),
+    Prod: document.querySelector("[data-modalProd]"),
+  };
 
-//   refs.openProdBtn.addEventListener("click", toggleModal);
-//   refs.closeProdBtn.addEventListener("click", toggleModal);
+  refs.openProdBtn.addEventListener("click", toggleModal);
+  refs.closeProdBtn.addEventListener("click", toggleModal);
 
-//   function toggleModal() {
-//     document.body.classList.toggle("modalProd-open");
-//     refs.Prod.classList.toggle("is-hiddenProd");
-//   }
-// })();
+  function toggleModal() {
+    document.body.classList.toggle("modalProd-open");
+    refs.Prod.classList.toggle("is-hiddenProd");
+  }
+})();
